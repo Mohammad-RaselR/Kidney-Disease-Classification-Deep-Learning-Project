4af68cac85c8ef0016815ddfff0e6a900563e52c
Mohammad-RaselR <mrhrasel229@gmail.com>
2026-02-09 23:56:30 +0600

    Prepare_base_model added

--- src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -1,31 +1,31 @@
 from cnnClassifier.config.configuration import ConfigurationManager
-from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier import logger 
+from cnnClassifier.components.prepare_base_model import PrepareBaseModel
+STAGE_NAME="Prepare Base Model"
 
-STAGE_NAME="Data Ingestion Stage "
-
-class DataIngestionTrainingPipeline:
+class PrepareBaseModelTrainingPipeline():
     def __init__(self):
         pass 
     
     def main(self):
 
         config= ConfigurationManager()
-        data_ingestion_config=config.get_data_ingestion_config()
+        prepare_base_model_config=config.get_prepare_base_model_config()
 
-        data_ingestion=DataIngestion(config=data_ingestion_config)
+        
+        prepare_base_model=PrepareBaseModel(config=prepare_base_model_config)
     
-        data_ingestion.download_file()
+        prepare_base_model.get_base_model()
 
-        data_ingestion.extract_zip_file()
-        
-        
+        prepare_base_model.update_base_model()
+
+
 if __name__=='__main__':
     try:
         logger.info(f">>>>stage {STAGE_NAME} started <<<<<")
-        obj=DataIngestionTrainingPipeline()
+        obj=PrepareBaseModelPipeline()
         obj.main()
         logger.info(f">>>>stage {STAGE_NAME} completed <<<<<\n\nx=========x")
     except Exception as e:
         logger.exception(e)
-        raise e; 
+        raise e;
